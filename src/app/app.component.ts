adc3e7bd7785a34cfd3af2abba36bc204aec5dcf
vamseeuxd <vamsee.uxd@gmail.com>
2021-02-15 07:52:14 +0530

    Day 1 Angualr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-firebase-feb';
+  appTitle: string = 'NG Firebase Feb 2021 ABC';
+  appNo: number = 1234;
+
+  getSomeData(val1: string, val2: number): string {
+    return `sample Data ${val1} ${val2}`;
+  }
 }
