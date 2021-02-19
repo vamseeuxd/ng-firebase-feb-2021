c9233eb0583b17fd38fee0a1ad92598dd41b9fa2
vamseeuxd <vamsee.uxd@gmail.com>
2021-02-19 09:34:18 +0530

    added Components ( Header, Footer, Main Container and Sidebar )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './global-components/header/header.component';
+import { FooterComponent } from './global-components/footer/footer.component';
+import { SidebarComponent } from './global-components/sidebar/sidebar.component';
+import { MainContainerComponent } from './global-components/main-container/main-container.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    MainContainerComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
